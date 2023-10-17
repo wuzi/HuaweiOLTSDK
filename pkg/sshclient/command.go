2571e15556ec33fa5ff6891319ea2a7ad68f2c92
Lucas Godoy <lcsdegodoy@outlook.com>
2023-10-17 20:22:24 -0300

    add ont commands

--- pkg/sshclient/command.go
+++ pkg/sshclient/command.go
@@ -1,13 +1,17 @@
 package sshclient
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 func (c *Client) Enable() error {
 	output, err := c.RunCommand("enable", "MA5683T#")
 	if err != nil {
 		return fmt.Errorf("failed to run command enable: %v", err)
 	}
-	c.Level = 0
+	c.Context.Level = 0
 	fmt.Print(output)
 	return nil
 }
@@ -17,7 +21,7 @@ func (c *Client) Config() error {
 	if err != nil {
 		return fmt.Errorf("failed to run command config: %v", err)
 	}
-	c.Level = 1
+	c.Context.Level = 1
 	fmt.Print(output)
 	return nil
 }
@@ -26,7 +30,7 @@ func (c *Client) Quit() error {
 	var output string
 	var err error
 
-	if c.Level >= 2 {
+	if c.Context.Level >= 2 {
 		output, err = c.RunCommand("quit", "MA5683T(config)#")
 		if err != nil {
 			return fmt.Errorf("failed to run command: %v", err)
@@ -34,7 +38,7 @@ func (c *Client) Quit() error {
 		fmt.Print(output)
 	}
 
-	if c.Level >= 1 {
+	if c.Context.Level >= 1 {
 		output, err = c.RunCommand("quit", "MA5683T#")
 		if err != nil {
 			return fmt.Errorf("failed to run command: %v", err)
@@ -56,7 +60,7 @@ func (c *Client) Quit() error {
 	return nil
 }
 
-func (c *Client) DisplayUnmanagedONT() ([]ONT, error) {
+func (c *Client) DisplayUnmanagedOnt() ([]ONT, error) {
 	output, err := c.RunCommand("display ont autofind all", "MA5683T(config)#")
 	if err != nil {
 		return nil, fmt.Errorf("failed to run command: %v", err)
@@ -70,7 +74,49 @@ func (c *Client) InterfaceGPON(frame int, slot int) error {
 	if err != nil {
 		return fmt.Errorf("failed to run command: %v", err)
 	}
-	c.Level = 2
+	c.Context.Level = 2
+	c.Context.Frame = frame
+	c.Context.Slot = slot
+	fmt.Print(output)
+	return nil
+}
+
+func (c *Client) AddOnt(port int, serialNumber string, description string) (string, error) {
+	output, err := c.RunCommand(fmt.Sprintf("ont add %d sn-auth %s omci ont-lineprofile-id 60 ont-srvprofile-id 35 desc %s",
+		port,
+		serialNumber,
+		description,
+	), fmt.Sprintf("MA5683T(config-if-gpon-%d/%d)#", c.Context.Frame, c.Context.Slot))
+
+	if err != nil {
+		return "", fmt.Errorf("failed to run command: %v", err)
+	}
+	fmt.Print(output)
+
+	if strings.Contains(output, "Failure: SN already exists") {
+		return "", fmt.Errorf("serial number already exists")
+	}
+
+	re := regexp.MustCompile(`ONTID :(\d+)`)
+	match := re.FindStringSubmatch(output)
+	if len(match) < 2 {
+		return "", fmt.Errorf("ONTID not found in command output")
+	}
+
+	return match[1], nil
+}
+
+func (c *Client) DeleteOnt(port int) error {
+	output, err := c.RunCommand(fmt.Sprintf("ont delete %d all", port), "(y/n)[n]:")
+	if err != nil {
+		return fmt.Errorf("failed to run command: %v", err)
+	}
+	fmt.Print(output)
+
+	output, err = c.RunCommand("y", fmt.Sprintf("MA5683T(config-if-gpon-%d/%d)#", c.Context.Frame, c.Context.Slot))
+	if err != nil {
+		return fmt.Errorf("failed to run command: %v", err)
+	}
 	fmt.Print(output)
 	return nil
 }
