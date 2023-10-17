927d56fd199911bed50b73cd575e4b36da864ed2
Lucas Godoy <lcsdegodoy@outlook.com>
2023-10-17 20:29:09 -0300

    add checks before running commands

--- pkg/sshclient/command.go
+++ pkg/sshclient/command.go
@@ -7,21 +7,27 @@ import (
 )
 
 func (c *Client) Enable() error {
+	if c.Context.Level != 0 {
+		return fmt.Errorf("not in root mode")
+	}
 	output, err := c.RunCommand("enable", "MA5683T#")
 	if err != nil {
 		return fmt.Errorf("failed to run command enable: %v", err)
 	}
-	c.Context.Level = 0
+	c.Context.Level = 1
 	fmt.Print(output)
 	return nil
 }
 
 func (c *Client) Config() error {
+	if c.Context.Level != 1 {
+		return fmt.Errorf("not in enable mode")
+	}
 	output, err := c.RunCommand("config", "MA5683T(config)#")
 	if err != nil {
 		return fmt.Errorf("failed to run command config: %v", err)
 	}
-	c.Context.Level = 1
+	c.Context.Level = 2
 	fmt.Print(output)
 	return nil
 }
@@ -30,7 +36,7 @@ func (c *Client) Quit() error {
 	var output string
 	var err error
 
-	if c.Context.Level >= 2 {
+	if c.Context.Level >= 3 {
 		output, err = c.RunCommand("quit", "MA5683T(config)#")
 		if err != nil {
 			return fmt.Errorf("failed to run command: %v", err)
@@ -38,7 +44,7 @@ func (c *Client) Quit() error {
 		fmt.Print(output)
 	}
 
-	if c.Context.Level >= 1 {
+	if c.Context.Level >= 2 {
 		output, err = c.RunCommand("quit", "MA5683T#")
 		if err != nil {
 			return fmt.Errorf("failed to run command: %v", err)
@@ -61,6 +67,9 @@ func (c *Client) Quit() error {
 }
 
 func (c *Client) DisplayUnmanagedOnt() ([]ONT, error) {
+	if c.Context.Level != 2 {
+		return nil, fmt.Errorf("not in config mode")
+	}
 	output, err := c.RunCommand("display ont autofind all", "MA5683T(config)#")
 	if err != nil {
 		return nil, fmt.Errorf("failed to run command: %v", err)
@@ -70,11 +79,14 @@ func (c *Client) DisplayUnmanagedOnt() ([]ONT, error) {
 }
 
 func (c *Client) InterfaceGPON(frame int, slot int) error {
+	if c.Context.Level != 2 {
+		return fmt.Errorf("not in config mode")
+	}
 	output, err := c.RunCommand(fmt.Sprintf("interface gpon %d/%d", frame, slot), fmt.Sprintf("MA5683T(config-if-gpon-%d/%d)#", frame, slot))
 	if err != nil {
 		return fmt.Errorf("failed to run command: %v", err)
 	}
-	c.Context.Level = 2
+	c.Context.Level = 3
 	c.Context.Frame = frame
 	c.Context.Slot = slot
 	fmt.Print(output)
@@ -82,6 +94,10 @@ func (c *Client) InterfaceGPON(frame int, slot int) error {
 }
 
 func (c *Client) AddOnt(port int, serialNumber string, description string) (string, error) {
+	if c.Context.Level != 3 {
+		return "", fmt.Errorf("not in interface gpon mode")
+	}
+
 	output, err := c.RunCommand(fmt.Sprintf("ont add %d sn-auth %s omci ont-lineprofile-id 60 ont-srvprofile-id 35 desc %s",
 		port,
 		serialNumber,
@@ -107,6 +123,10 @@ func (c *Client) AddOnt(port int, serialNumber string, description string) (stri
 }
 
 func (c *Client) DeleteOnt(port int) error {
+	if c.Context.Level != 3 {
+		return fmt.Errorf("not in interface gpon mode")
+	}
+
 	output, err := c.RunCommand(fmt.Sprintf("ont delete %d all", port), "(y/n)[n]:")
 	if err != nil {
 		return fmt.Errorf("failed to run command: %v", err)
