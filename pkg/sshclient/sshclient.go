f474b98f08ff5da7154830bd8260b07a334cdb60
Lucas Godoy <lcsdegodoy@outlook.com>
2024-02-06 09:23:22 -0300

    add json tags for ont structs

--- pkg/sshclient/sshclient.go
+++ pkg/sshclient/sshclient.go
@@ -1,27 +1,27 @@
 package sshclient
 
 type ONT struct {
-	ID           string
-	Frame        string
-	Slot         string
-	Port         string
-	SerialNumber string
-	VlanID       string
-	Description  string
-	ServicePort  string
+	ID           string `json:"id"`
+	Frame        string `json:"frame"`
+	Slot         string `json:"slot"`
+	Port         string `json:"port"`
+	SerialNumber string `json:"serial_number"`
+	VlanID       string `json:"vlan_id"`
+	Description  string `json:"description"`
+	ServicePort  string `json:"service_port"`
 }
 
 type ONTDetail struct {
-	Number             string
-	FSP                string
-	OntSN              string
-	Password           string
-	Loid               string
-	Checkcode          string
-	VendorID           string
-	OntVersion         string
-	OntSoftwareVersion string
-	OntEquipmentID     string
-	OntCustomizedInfo  string
-	OntAutofindTime    string
+	Number             string `json:"number"`
+	FSP                string `json:"fsp"`
+	OntSN              string `json:"serial_number"`
+	Password           string `json:"password"`
+	Loid               string `json:"lo_id"`
+	Checkcode          string `json:"check_code"`
+	VendorID           string `json:"vendor_id"`
+	OntVersion         string `json:"version"`
+	OntSoftwareVersion string `json:"software_version"`
+	OntEquipmentID     string `json:"equipment_id"`
+	OntCustomizedInfo  string `json:"customized_info"`
+	OntAutofindTime    string `json:"auto_find_time"`
 }
