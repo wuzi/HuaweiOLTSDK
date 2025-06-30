ae3c5c842477d0a75412cbd3db938dc68e3b2299
Lucas Godoy <lcsdegodoy@outlook.com>
2025-06-30 14:21:39 -0300

    update ParseUnmanagedONT to map by prefix and not line

--- pkg/sshclient/parser.go
+++ pkg/sshclient/parser.go
@@ -29,39 +29,47 @@ func (o *UnmanagedONT) GetFrameSlotPort() (int, int, int, error) {
 
 func ParseUnmanagedONT(output string) ([]UnmanagedONT, error) {
 	results := make([]UnmanagedONT, 0)
-
 	sections := strings.Split(output, "   ----------------------------------------------------------------------------")
+
+	fieldMap := []struct {
+		Prefix string
+		Set    func(*UnmanagedONT, string)
+	}{
+		{"Number              :", func(o *UnmanagedONT, v string) { o.Number = v }},
+		{"F/S/P               :", func(o *UnmanagedONT, v string) { o.FSP = v }},
+		{"Ont SN              :", func(o *UnmanagedONT, v string) { o.OntSN = v }},
+		{"Password            :", func(o *UnmanagedONT, v string) { o.Password = v }},
+		{"Loid                :", func(o *UnmanagedONT, v string) { o.Loid = v }},
+		{"Checkcode           :", func(o *UnmanagedONT, v string) { o.Checkcode = v }},
+		{"VendorID            :", func(o *UnmanagedONT, v string) { o.VendorID = v }},
+		{"Ont Version         :", func(o *UnmanagedONT, v string) { o.OntVersion = v }},
+		{"Ont SoftwareVersion :", func(o *UnmanagedONT, v string) { o.OntSoftwareVersion = v }},
+		{"Ont EquipmentID     :", func(o *UnmanagedONT, v string) { o.OntEquipmentID = v }},
+		{"Ont Customized Info :", func(o *UnmanagedONT, v string) { o.OntCustomizedInfo = v }},
+		{"Ont autofind time   :", func(o *UnmanagedONT, v string) { o.OntAutofindTime = v }},
+	}
 
 	for _, section := range sections[1:] {
 		lines := strings.Split(section, "\n")
+		var ont UnmanagedONT
 
-		var cleanLines []string
 		for _, line := range lines {
-			if line = strings.TrimSpace(line); line != "" && !strings.Contains(line, "---- More (") {
-				cleanLines = append(cleanLines, line)
+			cleanLine := strings.TrimSpace(line)
+			if cleanLine == "" || strings.Contains(cleanLine, "---- More (") {
+				continue
+			}
+			for _, fm := range fieldMap {
+				if strings.HasPrefix(cleanLine, fm.Prefix) {
+					value := strings.TrimSpace(strings.TrimPrefix(cleanLine, fm.Prefix))
+					fm.Set(&ont, value)
+					break
+				}
 			}
 		}
 
-		if len(cleanLines) < 11 {
-			continue
+		if ont.Number != "" {
+			results = append(results, ont)
 		}
-
-		ont := UnmanagedONT{
-			Number:             strings.TrimSpace(strings.TrimPrefix(cleanLines[0], "Number              :")),
-			FSP:                strings.TrimSpace(strings.TrimPrefix(cleanLines[1], "F/S/P               :")),
-			OntSN:              strings.TrimSpace(strings.TrimPrefix(cleanLines[2], "Ont SN              :")),
-			Password:           strings.TrimSpace(strings.TrimPrefix(cleanLines[3], "Password            :")),
-			Loid:               strings.TrimSpace(strings.TrimPrefix(cleanLines[4], "Loid                :")),
-			Checkcode:          strings.TrimSpace(strings.TrimPrefix(cleanLines[5], "Checkcode           :")),
-			VendorID:           strings.TrimSpace(strings.TrimPrefix(cleanLines[6], "VendorID            :")),
-			OntVersion:         strings.TrimSpace(strings.TrimPrefix(cleanLines[7], "Ont Version         :")),
-			OntSoftwareVersion: strings.TrimSpace(strings.TrimPrefix(cleanLines[8], "Ont SoftwareVersion :")),
-			OntEquipmentID:     strings.TrimSpace(strings.TrimPrefix(cleanLines[9], "Ont EquipmentID     :")),
-			OntCustomizedInfo:  strings.TrimSpace(strings.TrimPrefix(cleanLines[10], "Ont Customized Info :")),
-			OntAutofindTime:    strings.TrimSpace(strings.TrimPrefix(cleanLines[11], "Ont autofind time   :")),
-		}
-
-		results = append(results, ont)
 	}
 
 	return results, nil
